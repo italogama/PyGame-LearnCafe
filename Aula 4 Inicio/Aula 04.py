c4d53d36943a77cfe051a63470cc0f6daa346ee0
Italo Gama <ytalo_gama@hotmail.com>
2018-10-23 22:33:31 -0300

    Aula 07 - Cores de fundo e timer

--- Aula 4 Inicio/Aula 04.py
+++ Aula 4 Inicio/Aula 04.py
@@ -1,9 +1,12 @@
 import pygame
 
 def main(): #parametros de Display
+    #As definições dos objetos(variaveis)
     pygame.init() #Inicializando o pygame
-    pygame.display.set_mode([300, 300]) #Setando a dimensao da tela
+    tela = pygame.display.set_mode([300, 300]) #Setando a dimensao da tela
     pygame.display.set_caption("Iniciando com PyGame") #Exibindo titulo do game
+    relogio = pygame.time.Clock()
+    cor_branca = (255,255,255)
     
     sair = False
 
@@ -11,9 +14,10 @@ def main(): #parametros de Display
         for event in pygame.event.get(): #ao clicar no X ele entra na condição IF
             if event.type == pygame.QUIT: #se o evento recebido for o QUIT, o sair passa a ser True
                 sair = True #no momento que o sair passa a ser True, ele sai do while e executa o Quit
-
-    
-    pygame.display.update() #Chamar atualização na Tela
+        relogio.tick(27) #tick pra atualizar
+        tela.fill(cor_branca) #preencher o fundo da tela com cor branca
+        pygame.display.update() #Chamar atualização na Tela
+        
     pygame.quit() #Quita o game ao clicar no X
         
         
