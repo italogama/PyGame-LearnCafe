3836b54d7458faa507e6a010c3c02db0ab2c9b78
italo.gama <italo.gama@mv.com.br>
2018-11-05 13:42:33 -0200

    Aula 26 - Criando jogo com Pygame

--- Aula 4 Inicio/Aula 20.py
+++ Aula 4 Inicio/Aula 20.py
@@ -21,7 +21,8 @@ def main(): #parametros de Display
     ret5 = pygame.Rect(45, 170, 555, 6)
     ret6 = pygame.Rect(10, 220, 400, 6)
     ret7 = pygame.Rect(445, 220, 145, 6)
-    ret8 = pygame.Rect(45, 170, 555, 6)
+    ret8 = pygame.Rect(45, 270, 555, 6)
+    ret9 = pygame.Rect(10, 320, 545, 6)
     sair = False
 
     pygame.font.init()
@@ -50,7 +51,7 @@ def main(): #parametros de Display
         (ret.left, ret.top) = pygame.mouse.get_pos() #Evento de captura do mouse
         ret.left -= ret.width/2   #Coloca cursos no centro do objeto ret
         ret.top -= ret.height/2
-        if ret.colliderect(ret2) or ret.colliderect(ret3) or ret.colliderect(ret4): #testando colisão com objeto
+        if ret.colliderect(ret2) or ret.colliderect(ret3) or ret.colliderect(ret4) or ret.colliderect(ret5) or ret.colliderect(ret6) or ret.colliderect(ret7) or ret.colliderect(ret8) or ret.colliderect(ret9): #testando colisão com objeto
             text = fonte_perdeu.render('VOCE PERDEU', 1, (cor_branca)) #printando perdeu 
             tela.blit(text, (200, 200))
             pygame.mouse.set_pos(10, 10)
@@ -58,12 +59,12 @@ def main(): #parametros de Display
             audio_explosao.set_volume(00.1)
             (ret.left, ret.top) = (xant, yant)
 
-        if ret.top > 400:
+        if ret.top > 330:
             text = fonte_ganhou.render('VOCE GANHOU', 1, (cor_branca)) #printando perdeu 
             tela.blit(text, (200, 200))
             text = fonte_perdeu.render('CLIQUE PARA RECOMECAR', 1, (cor_vermelha)) #printando perdeu 
             tela.blit(text, (150, 250))
-            ret2.left = 602
+            ret2.left, ret3.left = 602
 
             
         pygame.draw.rect(tela, cor_vermelha, ret) #inserindo retangulo
@@ -73,6 +74,8 @@ def main(): #parametros de Display
         pygame.draw.rect(tela, cor_rosa, ret5)
         pygame.draw.rect(tela, cor_rosa, ret6)
         pygame.draw.rect(tela, cor_rosa, ret7)
+        pygame.draw.rect(tela, cor_rosa, ret8)
+        pygame.draw.rect(tela, cor_rosa, ret9)
         pygame.display.update() #Chamar atualização na Tela
         
     pygame.quit() #Quita o game ao clicar no X
