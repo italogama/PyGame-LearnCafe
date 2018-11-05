7afa541820b03cf05bc0c0aa87679a5b27ff0362
italo.gama <italo.gama@mv.com.br>
2018-11-05 13:32:03 -0200

    Aula 25 - Criando jogo com Pygame

--- Aula 4 Inicio/Aula 20.py
+++ Aula 4 Inicio/Aula 20.py
@@ -15,9 +15,13 @@ def main(): #parametros de Display
     sup.fill(cor_azul)
     
     ret = pygame.Rect(10, 10, 30, 30)
-    ret2 = pygame.Rect(10, 40, 555, 6)
-    ret3 = pygame.Rect(10, 90, 355, 6)
-    ret4 = pygame.Rect(405, 90, 195, 6)
+    ret2 = pygame.Rect(10, 70, 555, 6)
+    ret3 = pygame.Rect(10, 120, 350, 6)
+    ret4 = pygame.Rect(405, 120, 195, 6)
+    ret5 = pygame.Rect(45, 170, 555, 6)
+    ret6 = pygame.Rect(10, 220, 400, 6)
+    ret7 = pygame.Rect(445, 220, 145, 6)
+    ret8 = pygame.Rect(45, 170, 555, 6)
     sair = False
 
     pygame.font.init()
@@ -46,7 +50,7 @@ def main(): #parametros de Display
         (ret.left, ret.top) = pygame.mouse.get_pos() #Evento de captura do mouse
         ret.left -= ret.width/2   #Coloca cursos no centro do objeto ret
         ret.top -= ret.height/2
-        if ret.colliderect(ret2) or ret.colliderect(ret3): #testando colisão com objeto
+        if ret.colliderect(ret2) or ret.colliderect(ret3) or ret.colliderect(ret4): #testando colisão com objeto
             text = fonte_perdeu.render('VOCE PERDEU', 1, (cor_branca)) #printando perdeu 
             tela.blit(text, (200, 200))
             pygame.mouse.set_pos(10, 10)
@@ -65,7 +69,10 @@ def main(): #parametros de Display
         pygame.draw.rect(tela, cor_vermelha, ret) #inserindo retangulo
         pygame.draw.rect(tela, cor_rosa, ret2)
         pygame.draw.rect(tela, cor_rosa, ret3)
-        pygame.draw.rect(tela, cor_rosa, ret4)        
+        pygame.draw.rect(tela, cor_rosa, ret4)
+        pygame.draw.rect(tela, cor_rosa, ret5)
+        pygame.draw.rect(tela, cor_rosa, ret6)
+        pygame.draw.rect(tela, cor_rosa, ret7)
         pygame.display.update() #Chamar atualização na Tela
         
     pygame.quit() #Quita o game ao clicar no X
