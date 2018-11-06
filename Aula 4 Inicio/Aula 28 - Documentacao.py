03df5a0f30cd5eecbd823033445ca66610632535
Khayrus <ytalo_gama@hotmail.com>
2018-11-05 21:54:06 -0300

    Aula 30 - Criando um contador

--- Aula 4 Inicio/Aula 28 - Documentacao.py
+++ Aula 4 Inicio/Aula 28 - Documentacao.py
@@ -4,7 +4,7 @@ import random
 def main(): #parametros de Display
     #As definições dos objetos(variaveis)
     pygame.init() #Inicializando o pygame
-    tela = pygame.display.set_mode([300, 300]) #Setando a dimensao da tela
+    tela = pygame.display.set_mode([600, 600]) #Setando a dimensao da tela
     pygame.display.set_caption("Iniciando com PyGame") #Exibindo titulo do game
     relogio = pygame.time.Clock()
     cor_branca = (255,255,255)
@@ -12,7 +12,7 @@ def main(): #parametros de Display
     cor_verde = (54,182,112)
     cor_vermelha = (227,57,9)
     cor_rosa = (253, 147, 226)
-    sup = pygame.Surface((280, 280))
+    sup = pygame.Surface((600, 600))
     sup.fill(cor_azul)
 
     sup2 = pygame.Surface((100, 100))
@@ -29,22 +29,24 @@ def main(): #parametros de Display
     font_padrao = pygame.font.get_default_font()
     fonte_perdeu = pygame.font.SysFont(font_padrao, 45)
     fonte_ganhou = pygame.font.SysFont(font_padrao, 30)
+    texto = pygame.font.SysFont("Arial", 30, True, False)
+    
 
     audio_explosao = pygame.mixer.Sound('AUDIOS.ogg')
 
     listaret = []
 
     for x in range(15):
-        h = random.randrange(20, 60)
-        w = random.randrange(15, 45)
-        x = random.randrange(280)
-        y = random.randrange(280)
+        h = random.randrange(20, 90)
+        w = random.randrange(15, 50)
+        x = random.randrange(600)
+        y = random.randrange(600)
         listaret.append(pygame.Rect(x, y, w, h))
         
     while sair != True: #Loop de while, cria uma variavel do tipo event que recebe um evento
 
         for rets in listaret:
-            pygame.draw.rect(sup, cor_azul, rets)
+            pygame.draw.rect(sup, cor_verde, rets)
 
 
         for event in pygame.event.get(): #ao clicar no X ele entra na condição IF
@@ -82,7 +84,7 @@ def main(): #parametros de Display
 
         relogio.tick(30) #tick pra atualizar
         tela.fill(cor_branca) #preencher o fundo da tela com cor branca
-        tela.blit(sup, [50,50]) #chamando a superficie
+        tela.blit(sup, [0,0]) #chamando a superficie
         tela.blit(sup2, [250,50]) #chamando a superficie 2
         tela.blit(sup2, [250,150])
         
@@ -97,6 +99,11 @@ def main(): #parametros de Display
             audio_explosao.set_volume(00.1)
             tela.blit(text, (150, 150))
             (ret.left, ret.top) = (xant, yant)'''
+
+        segundos = pygame.time.get_ticks()/1000
+        segundos = str(segundos)
+        contador = texto.render(segundos, 0, cor_branca)
+        sup.blit(contador, (300,10))
         
         pygame.draw.rect(tela, cor_vermelha, ret) #inserindo retangulo
         pygame.draw.rect(tela, cor_rosa, ret2)
