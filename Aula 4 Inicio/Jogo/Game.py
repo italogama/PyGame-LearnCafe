c9c64c06246d1a2664a177664838fd5a7b11057c
italo.gama <italo.gama@mv.com.br>
2018-11-06 11:15:22 -0200

    Aula 39 - Iniciando com a tela do jogo

--- Aula 4 Inicio/Jogo/Game.py
+++ Aula 4 Inicio/Jogo/Game.py
@@ -4,38 +4,36 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, imagem):
         self.imagem = imagem
         self.rect = self.imagem.get_rect()
-        self.rect.top, self.rect.left = (100, 200)
+        self.rect.top, self.rect.left = (200, 200) #posicao da imagem
 
     def mover(self, vx, vy):
         pass
 
-    def update(self, superficie):
-        superficie.blit(self.imagem, self.rect)
+    def update(self, superficie): #parametro de update
+        superficie.blit(self.imagem, self.rect) #qnd chamar update ele mostra esses dois objetos
 
-    def main():
-        import pygame
-        pygame.init()
-        tela = pygame.display.set_mode((480, 300))
-        sair = False
-        relogio = pygame.time.Clock()
+def main():
+    import pygame
+    pygame.init()
+    tela = pygame.display.set_mode((480, 300)) #tamanho da area que vai utilizar
+    sair = False
+    relogio = pygame.time.Clock()
 
-        img_nave = pygame.image.load("imagens/nave.png").convert_alpha()
-        jogador = Player(img_nave)
+    img_nave = pygame.image.load("../imagens/nave.png").convert_alpha()
+    jogador = Player(img_nave)
+
+    while sair != True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sair = True
+
+        relogio.tick(20)
+        tela.fill((200, 200, 200))
+        jogador.update(tela)
+
+        pygame.display.update()
 
 
-        while sair != True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sair = True
-
-            relogio.tick(20)
-            tela.fill((200, 200, 200))
-            jogador.update(tela)
-
-            pygame.display.update()
-
-
-        pygame.quit()
-
+    pygame.quit()
 
 main()
