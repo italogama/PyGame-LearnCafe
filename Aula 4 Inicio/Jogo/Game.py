56512ccadb4a2c3251e0d4025a4affebafcccfde
italo.gama <italo.gama@mv.com.br>
2018-11-06 11:00:33 -0200

    Aula 38 - Criando a tela do jogo

--- Aula 4 Inicio/Jogo/Game.py
+++ Aula 4 Inicio/Jogo/Game.py
@@ -9,15 +9,33 @@ class Player(pygame.sprite.Sprite):
     def mover(self, vx, vy):
         pass
 
-
     def update(self, superficie):
-        pass
+        superficie.blit(self.imagem, self.rect)
 
     def main():
         import pygame
         pygame.init()
-        tela = pygame.display.set_mode(480, 300))
+        tela = pygame.display.set_mode((480, 300))
         sair = False
         relogio = pygame.time.Clock()
 
         img_nave = pygame.image.load("imagens/nave.png").convert_alpha()
+        jogador = Player(img_nave)
+
+
+        while sair != True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sair = True
+
+            relogio.tick(20)
+            tela.fill((200, 200, 200))
+            jogador.update(tela)
+
+            pygame.display.update()
+
+
+        pygame.quit()
+
+
+main()
